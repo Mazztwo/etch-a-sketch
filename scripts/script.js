aa1e38d1cd67286a259a73c8c9ef896d44aa808f
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2017-02-15 18:50:39 -0500

    Added grid to main body using jQuery.

--- scripts/script.js
+++ scripts/script.js
@@ -4,12 +4,18 @@ $(document).ready(function()
 	var $grid = $("#grid");
 	var gridSize = 4;
 
-	for(var i  = 0; i < gridSize; i = i + 1)
+	for(var i  = 1; i <= gridSize; i = i + 1)
 	{
-		for(var k = 0; i < gridSize; k = k + 1)
+		$grid.append("<tr id='" + i + "'></tr>");
+
+
+		for(var k = 1; k <= gridSize; k = k + 1)
 		{
-			$grid.append("<div class='gridPixel'></div>");
+			$("#" + i).append("<td class='gridPixel'></td>");	
 		}
+		
+
+
 	}
 
 
