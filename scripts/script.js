fe1638a870e677dbc08004170097d431f8d20e8d
Alessio Mazzone <alessio.mazzone12345@gmail.com>
2017-02-17 20:05:58 -0500

    Implemented pixel density functionality via button.

--- scripts/script.js
+++ scripts/script.js
@@ -26,4 +26,33 @@ $(document).ready(function()
 		$(".gridPixel").removeClass("visited");
 	});
 
+	$("#changePixelButton").click(function()
+	{
+		gridSize = prompt("Please enter a grid size X (Resulting grid will be X by X.", "grid size");
+		$("tr").remove();
+		var newWidth = 500/gridSize;
+		$(".gridPixel").width(newWidth);
+
+
+		for(var i  = 1; i <= gridSize; i = i + 1)
+		{
+			$grid.append("<tr id='" + i + "'></tr>");
+
+			for(var k = 1; k <= gridSize; k = k + 1)
+			{
+				$("#" + i).append("<td class='gridPixel'></td>");	
+			}
+		}
+	});
+
 });
+
+$("td.gridPixel").mouseenter(function()
+	{
+		$(this).addClass("visited");
+
+	});
+
+
+
+
